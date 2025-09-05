d6d8c662acaa3d8ce86b8b803ea5dac9454c8072
Jenloke Magbojos <mjenloke@gmail.com>
2025-09-05 17:44:53 +0800

    update index route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
-        path: '',
-        redirectTo: '/home',
-        pathMatch: 'full'
-    },
+    // {
+    //     path: '',
+    //     redirectTo: '/home',
+    //     pathMatch: 'full'
+    // },
     {
         path: 'home',
         loadComponent: () => import('./pages/home/home').then(c => c.Home)
